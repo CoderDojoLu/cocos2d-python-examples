f975c1f73524c401e479ed6c2b2d029dc4a8afb4
Steve Clement <steve@localhost.lu>
2015-10-17 11:13:22 +0200

    - very simple helloWorld example

---
+++ helloWorld.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import cocos
+
+class HelloWorld(cocos.layer.Layer):
+	def __init__(self):
+		super(HelloWorld, self).__init__()
+		label = cocos.text.Label('Hello, world', font_name='Times New Roman', font_size=32, anchor_x='center', anchor_y='center')
+		label.position = 320, 240
+		self.add(label)
+
+def main():
+	cocos.director.director.init()
+	hello_layer = HelloWorld()
+	main_scene = cocos.scene.Scene(hello_layer)
+	cocos.director.director.run(main_scene)
+
+if __name__ == '__main__': main()
